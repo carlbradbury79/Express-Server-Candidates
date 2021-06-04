a1d8af791b3ac9ea42ea84015fd7c6958ec7bbb7
Carl Bradbury <carl@gnarlydog.co.uk>
2021-06-04 21:45:01 +0100

    Replaced find with aggregate on the GET route

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Express Server
 
 Has two routes, POST and GET
+
+## POST
+
+The post requst checks the db doesn't have a candidate with same name (can't remember inital field to compare). If the user does not exist they are added.
+
+## GET
+
+Using aggregate to find the best candidates by comparing the query params and the skills field each candidate has. Each candidate gets a field with their total number of matches, which is then sorted. The one with the most matches (or the first in the list if there are two or more equal number of equal candicates) is sent back
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.post('/', async function (req, res) {
   // Get candidates details
   const { name, skills } = req.body;
   try {
-    //   Check if user exists
+    //   Check if user exists (by name)
     const userExists = await Candidate.findOne({ name });
 
     // User exists, throw error
@@ -43,35 +43,34 @@ app.post('/', async function (req, res) {
 app.get('/', async function (req, res) {
   // Turn query params into array
   const skillsArr = req.query.skills.split(',');
-  try {
-    //   Get all Candidates
-    const candidates = await Candidate.find();
-    let chosen = { name: '', matches: 0 };
 
-    //
-    candidates.forEach((c) => {
-      // Get matches for each candidate, comparing skills with Query params
-      const matches = c.skills.reduce((acc, cur) => {
-        if (skillsArr.includes(cur)) {
-          return (acc += 1);
-        } else {
-          return acc;
-        }
-      }, 0);
-      //   Change chosen candidate if has more matches
-      if (matches > chosen.matches) {
-        chosen = {
-          name: c.name,
-          matches: matches,
-        };
+  try {
+    //   Use aggregate to get candidates that...
+    //   Match skills in the array created above
+    //   A new field is created with total matching skills
+    //   The array of suitable candidates is sorted
+    //   With only the top three returned
+    await Candidate.aggregate([
+      { $unwind: '$skills' },
+      { $match: { skills: { $in: skillsArr } } },
+      {
+        $group: {
+          _id: '$_id',
+          name: { $first: '$name' },
+          matches: { $sum: 1 },
+        },
+      },
+      { $sort: { matches: -1 } },
+      { $limit: 3 },
+    ]).exec(function (err, candidates) {
+      // console.log(candidates);
+      if (err) res.sendStatus(500);
+      if (candidates.length < 1) {
+        res.sendStatus(404); //No matching skills for any candidate
+      } else {
+        res.send(candidates[0]).status(200); //Send best candidate
       }
     });
-    // Send candidate if one exists with at least one match
-    if (chosen.matches > 0) {
-      res.send(chosen).status(200);
-    } else {
-      res.sendStatus(404);
-    }
   } catch (error) {
     res.sendStatus(500);
   }
